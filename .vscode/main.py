4d15b7a5022e34887f2a4f250a18d2f9d2430437
An Pham <an.phxm@gmail.com>
2023-06-07 23:21:21 -0400

    able to print team standing

--- .vscode/main.py
+++ .vscode/main.py
@@ -36,6 +36,7 @@ def run():
         #schedule.every().thursday.at("10:00").do(asyncio.run, send_weekly_message)
 
         await send_weekly_message()
+        await weekly_standing()
 
     @bot.command(
             help="I am still under development!",
@@ -46,6 +47,37 @@ def run():
     async def ping(ctx):
         """ Answers with pong """
         await ctx.send("pong")
+    
+    async def weekly_standing():
+        channel = bot.get_channel(902777744478310401)
+       
+        data = []
+        message = ""
+        with open('.vscode\standing.txt', 'r') as file:
+            # Read each line
+            lines = file.readlines()
+
+            for line in lines:
+                fields = line.strip().split(',')
+                data.append(fields)
+
+        # Function to print the table
+        
+        headers = ['PLACE', 'TEAM', 'WIN', 'LOSS', 'TIE', 'POINTS', 'SPIRIT']
+        header_format = '{:<6} {:<20} {:<4} {:<5} {:<4} {:<10} {:<16}'
+
+        table = f"```{header_format.format(*headers)}\n"
+
+        for row in data:
+            table += f"{row[0]:<6} {row[1]:<20} {row[2]:<4} {row[3]:<5} {row[4]:<4} {row[5]:<10} {row[6]:<16}\n"
+            if row[1] == "Uppercuts":
+                message += f"We are currently in {row[0]}th place. Our spirit score is {row[6]}. KEEP IT UP!!"
+                
+        table += "```"
+    
+        # Call the function to print the table
+        await channel.send(table)
+        await channel.send(message)
 
     async def send_weekly_message():
 
