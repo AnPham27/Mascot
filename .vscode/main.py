f61b8ded8dc32a9d3a80a76766d04a5997a93f4f
An Pham <an.phxm@gmail.com>
2023-06-11 14:15:02 -0400

    added weekly_schedule command

--- .vscode/main.py
+++ .vscode/main.py
@@ -5,6 +5,9 @@ import random
 from datetime import date
 import schedule_scraper
 from standings_scraper import standings
+from schedule_scraper import get_schedule
+from datetime import date
+import calendar
 
 
 filename = ".vscode\schedule.txt"
@@ -40,7 +43,19 @@ def run():
     )
     async def schedule(ctx):
         """ Answers with the schedule for that day """
-        await send_weekly_message()
+        await weekly_schedule()
+
+    async def weekly_schedule():
+        channel = bot.get_channel(1112936855088943165)
+        # Get today's date
+        today = date.today()
+        d2 = today.strftime("%B %d")
+        today_date = f"{calendar.day_name[today.weekday()]}, {d2}"
+        today_date = "Thursday, May 18"
+        message = get_schedule(today_date)
+
+        await channel.send(message)
+
 
     @bot.command(
             help="I am still under development!",
@@ -60,45 +75,6 @@ def run():
         
         await channel.send(table)
         await channel.send(message)
-
-
-    async def send_weekly_message():
-
-        channel = bot.get_channel(1112936855088943165)
-
-        # Get today's date
-        today = date.today()
-
-        d2 = today.strftime("%B %d")
-
-        # Open the text file
-        with open(filename, 'r') as file:
-            # Read each line
-            lines = file.readlines()
-
-            # Process each line
-            for line in lines:
-                # Split the line into fields
-                fields = line.strip().split(',')
-
-                # Extract the fields
-                date_field = fields[0]
-
-                # Compare today's date with the date from the field
-                if d2 == date_field:
-                    # Extract the remaining fields
-                    team1 = fields[1]
-                    color1 = fields[2]
-                    field1 = fields[3]
-                    team2 = fields[4]
-                    color2 = fields[5]
-                    field2 = fields[6]
-
-                    # Form the sentence
-                    sentence = f"@everyone \nOn {date_field}, we are playing against **{team1}** wearing **{color1}** on **field #{field1}**. Next, we are playing against **{team2}** wearing **{color2}** on **field #{field2}**."
-                    
-                    await channel.send(sentence)
-
 
 
     bot.run(settings.DISCORD_API_SECRET, root_logger=True)
