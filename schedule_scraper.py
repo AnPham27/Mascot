ca03e8369f5fc75c761d98a487800e7299a8ae6d
An Pham <an.phxm@gmail.com>
2023-06-12 23:59:52 -0400

    playoff date except

--- schedule_scraper.py
+++ schedule_scraper.py
@@ -256,6 +256,8 @@ def get_upcoming_schedule(upcoming_date):
 
     if today_date in days:
         print("yes")
+    elif today_date == "Thursday, June 29":
+        return(f"We have playoffs that day, and there is no schedule for that yet.")
     else:
         return(f"There are no games on {today_date}. Check for a different date")
     
