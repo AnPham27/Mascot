af7955ab06e5f15fbce2098c85bf938ede9ea978
An Pham <an.phxm@gmail.com>
2023-06-24 18:31:41 -0400

    fix standings format for smaller screens

--- standings_scraper.py
+++ standings_scraper.py
@@ -60,7 +60,7 @@ def standings():
         place = 1
             #[team , W, L, T, P]
         for r in row:
-            chart += f"{place:<4} {r[0]:<17} {r[1]:<2} {r[2]:<2} {r[4]:<6}\n"
+            chart += f"{place:<3} {r[0]:<17} {r[1]:<2} {r[2]:<2} {r[4]:<6}\n"
 
             if r[0] == "Uppercuts":
                 message += f"We are currently in {place}th place. Our spirit score is still being calculated. KEEP IT UP!!"
@@ -89,7 +89,7 @@ def standings():
             
 
         headers = ['PL', 'TEAM', 'W', 'L', 'SPIRIT']
-        header_format = '{:<4} {:<17} {:<2} {:<2} {:<6}'
+        header_format = '{:<3} {:<17} {:<2} {:<2} {:<6}'
 
         chart = f"```\n{header_format.format(*headers)}\n"
         message = ""
