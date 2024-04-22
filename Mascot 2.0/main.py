92d16039b2838e35f4d1d65ec67db78fde1d4f6e
An Pham <an.phxm@gmail.com>
2024-04-22 01:07:52 -0400

    fixing comments in commands

--- Mascot 2.0/main.py
+++ Mascot 2.0/main.py
@@ -19,13 +19,15 @@ def run():
     #!upc day month date team num - (no @everyone) responds to same text channel with respective date and team number
     @bot.command(
             help="I am still under development!",
-            description="Posting the schedule",
+            description="Posting the schedule based on the league division, date, and team number",
             brief="Posts the schedule",
             hidden=True
     )
     async def upc(ctx, division, day, month, date, team_num):
-        """ Upcoming schedule to same channel: 
-        FORMAT: !upc c2 Thursday, July 5 4"""
+        """ 
+        Upcoming schedule to same channel: 
+        FORMAT: !upc c2 Thursday, July 5 4
+        """
         
         message = get_upcoming_schedule(division, day, month, date, team_num)
 
@@ -35,13 +37,15 @@ def run():
     #!st division - (no @everyone) responds to same text channel with current standings     
     @bot.command(
             help="I am still under development!",
-            description="Posting the standings",
+            description="Posting the standings for the league division",
             brief="Posts the standings",
             hidden=True
     )
     async def st(ctx, division):
-        """ Current standing to the same channel: 
-        FORMAT: !st c2"""
+        """
+        Current standing to the same channel: 
+        FORMAT: !st c2
+        """
         table, message = standings(division.lower().replace(' ', ''))
         await ctx.send(table)
         await ctx.send(message)
